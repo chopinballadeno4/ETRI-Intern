e2a9c1fddced68bc88b3ad71ff47b929ecb4717d
happybae <qo98333@naver.com>
2022-07-12 11:27:15 +0900

    sass setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-emotion`,
     // {
     //   resolve: `gatsby-source-filesystem`,
     //   options: {
@@ -25,5 +26,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: <https://gatsby.dev/offline>
     // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+
+        }
+      }
+    },
   ],
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import React, { FunctionComponent } from "react";
-import Text from "../components/Test";
+import { Link } from "gatsby";
 
 const IndexPage: FunctionComponent = function () {
-	return <Text text="Home" />;
+	return (
+		<div>
+			<Link to="/info/">To Info</Link>
+		</div>
+	);
 };
 
 export default IndexPage;
