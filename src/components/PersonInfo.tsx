33c035c28c5ba41842ba453a16de4a82d5bc3805
happybae <qo98333@naver.com>
2022-07-22 17:30:08 +0900

    folder restruct

--- src/components/PersonInfo.tsx
+++ src/components/PersonInfo.tsx
@@ -2,7 +2,7 @@ import "../scss/c-personInfo.scss";
 import React from "react";
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
 import { graphql } from "gatsby";
-import { IPersonListType, IPersonInfo } from "types/PersonInfo.types";
+import { IPersonListType, IPersonInfo } from "./types/PersonInfo.types";
 
 function PersonInfo({ name, contact, department, work }: IPersonInfo) {
 	return <div className="PersonInfo-wrapper"></div>;
--- src/components/PersonList.tsx
+++ src/components/PersonList.tsx
@@ -2,7 +2,7 @@ import "../scss/c-personInfo.scss";
 import React from "react";
 import { GatsbyImage, IGatsbyImageData } from "gatsby-plugin-image";
 import { graphql } from "gatsby";
-import { IPersonListType, IPersonInfo } from "types/PersonInfo.types";
+import { IPersonListType, IPersonInfo } from "./types/PersonInfo.types";
 import PersonInfo from "components/PersonInfo";
 
 interface IPersonList {
