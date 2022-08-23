897f382cc56c9472eda4707d485566afbf394802
happybae <qo98333@naver.com>
2022-08-23 11:32:45 +0900

    #1 header & footer

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -10,38 +10,40 @@ import {
 } from "gatsby-plugin-image";
 
 const Header: FunctionComponent = function () {
-	const [isDark, setIsDark] = useState(false);
 	useEffect(() => {
 		if (window.localStorage.getItem("isDark") === null) {
-			setIsDark(false);
-			document.querySelector(".root")?.classList.remove("dark");
+			if (document.querySelector(".root")?.classList.contains("dark")) {
+				document.querySelector(".root")?.classList.remove("dark");
+			}
 		} else {
-			setIsDark(true);
-			document.querySelector(".root")?.classList.add("dark");
+			if (!document.querySelector(".root")?.classList.contains("dark")) {
+				document.querySelector(".root")?.classList.add("dark");
+			}
 		}
 	}, []);
 
 	const darkmodeClick = (event: React.MouseEvent<HTMLButtonElement>) => {
 		event.preventDefault();
 		if (window.localStorage.getItem("isDark") === null) {
+			// set Darkmode
 			window.localStorage.setItem("isDark", "yes");
 			document.querySelector(".root")?.classList.add("dark");
 		} else {
+			// release Darkmode
 			window.localStorage.removeItem("isDark");
 			document.querySelector(".root")?.classList.remove("dark");
 		}
-		console.log(document.querySelector(".root")?.classList);
 	};
 
 	return (
-		<div className="header-wrapper">
-			<header className="header-div">
+		<div id="header-wrapper">
+			<header id="header-main">
 				<div>
 					<Link to={"/"} id="header-logo">
 						<StaticImage src="../../static/logo.png" alt="logo" />
 					</Link>
 				</div>
-				<nav className="header-link">
+				<nav id="header-link">
 					<Link to={"/research"}>
 						<span>Our research</span>
 					</Link>
@@ -57,8 +59,8 @@ const Header: FunctionComponent = function () {
 					<Link to={"/about"}>
 						<span>About</span>
 					</Link>
-					<button onClick={darkmodeClick} className="mode-change-button">
-						<FontAwesomeIcon icon={faMoon} className="darkmode-icon" />
+					<button onClick={darkmodeClick} id="header-button">
+						<FontAwesomeIcon icon={faMoon} id="darkmode-icon" />
 					</button>
 				</nav>
 			</header>
